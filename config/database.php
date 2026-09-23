25333b7d09128c78a6a8671d0b9e715e07c15f2b
ganazius <ganazius@gmail.com>
2026-09-23 14:56:30 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // deklarasi parameter koneksi database
 $host     = "localhost"; 			// server database, default “localhost” atau “127.0.0.1”
-$username = "mohjasin_dbuser"; 				// username database, default “root”
-$password = "PutuneMbahMohJasin*"; 					// password database, default kosong
-$database = "mohjasin_dulur"; 			// memilih database yang akan digunakan
+$username = "00"; 				// username database, default “root”
+$password = "00*"; 					// password database, default kosong
+$database = "00"; 			// memilih database yang akan digunakan
 
 // buat koneksi database
 $mysqli = mysqli_connect($host, $username, $password, $database);
